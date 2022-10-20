5dea7d22ae69e2c68aca877b536f9b9d6df64942
jlbuild <juliabuildbot@gmail.com>
2022-10-20 09:38:35 +0000

    cuQuantum_jll build 22.7.114+1

--- src/wrappers/x86_64-linux-gnu-cuda+11.0.jl
+++ src/wrappers/x86_64-linux-gnu-cuda+11.0.jl
@@ -4,8 +4,8 @@ export libcustatevec, libcutensornet
 using CUDA_Runtime_jll
 using CUTENSOR_jll
 JLLWrappers.@generate_wrapper_header("cuQuantum")
-JLLWrappers.@declare_library_product(libcustatevec, "libcustatevec.so.0")
-JLLWrappers.@declare_library_product(libcutensornet, "libcutensornet.so.0")
+JLLWrappers.@declare_library_product(libcustatevec, "libcustatevec.so.1")
+JLLWrappers.@declare_library_product(libcutensornet, "libcutensornet.so.1")
 function __init__()
     JLLWrappers.@generate_init_header(CUDA_Runtime_jll, CUTENSOR_jll)
     JLLWrappers.@init_library_product(
